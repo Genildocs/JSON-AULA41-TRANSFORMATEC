e9b01fc2b708a0d91fd3b21782f48d218c6e972d
Genildocs <genildocs@gmail.com>
2022-06-15 10:52:40 -0300

    Terceiro commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function getPizza(event){
+    event.preventDefault();
+
+    const data = new FormData(event.target)
+
+    const json_data = Object.fromEntries(data.entries())
+
+    form.taste = data.getAll('taste')
+
+    const pre = document.querySelector('pre') 
+
+    pre.innerText = JSON.stringify(json_data, null, 2)
+}
+
+const form = document.getElementById('pizza_form')
+
+form.addEventListener('submit', getPizza)
